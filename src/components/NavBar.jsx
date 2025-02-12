1809297dc517484e9272a1d03528957c69bb27d1
victor <victorzemail@gmail.com>
2025-02-11 20:20:56 -0600

    Updated resume icon and changes filetype

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import navIcon4 from "../assets/img/nav-icon4.svg";
-import resume from "../Victor_Oloyede_Resume.docx";
+import resume from "../Victor_Oloyede_Resume.pdf";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
